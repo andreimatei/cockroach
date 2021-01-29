f49ff6b9d617c89f6f600703b781839018d84cbd
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2021-01-29 00:03:18 -0500

    kv: route present-time reads to global_read follower replicas
    First commit from #59505.
    
    This commit updates the kv client routing logic to account for the new
    `LEAD_FOR_GLOBAL_READS` `RangeClosedTimestampPolicy` added in #59505. In
    enterprise builds, non-stale read-only requests to ranges with this
    closed timestamp policy can now be routed to follower replicas, just
    like stale read-only requests to normal ranges currently are.
    
    In addition to this main change, there are a few smaller changes in this
    PR that were hard to split out, so are included in this commit.
    
    First, non-transactional requests are no longer served by followers even
    if the follower replica detects that the request can be. Previously,
    non-transactional requests would never be routed intentionally to a
    follower replica, but `Replica.canServeFollowerRead` would allow them
    through if their timestamp was low enough. This change was made in order
    to keep the client and server logic in-sync and because this does not
    seem safe for non-transactional requests that get their timestamp
    assigned on the server. We're planning to remove non-transactional
    requests soon anyway (#58459), so this isn't a big deal. It mostly
    just caused some testing fallout.
    
    Second, transactional requests that had previously written intents are
    now allowed to have their read-only requests served by followers, as
    long as those followers have a closed timestamp above the transaction's
    read *and* write timestamp. Previously, we had avoided this because it
    seemed to cause issues with read-your-writes. However, it appears safe
    as long as the write timestamp is below the closed timestamp, because we
    know all of the transaction's intents are at or below its write
    timestamp. This is very important for multi-region work, where we want a
    transaction to be able to write to a REGIONAL table and then later
    perform local reads (maybe foreign key checks) on GLOBAL tables.
    
    Third, a max clock offset shift in `canUseFollowerRead` was removed. It
    wasn't exactly clear what this was doing. It was introduced in the
    original 70be833 and seemed to allow a follower read in cases where they
    otherwise shouldn't be expected to succeed. I thought at first that it
    was accounting for the fact that the kv client's clock might be leading
    the kv server's clock and so it was being pessimistic about the expected
    closed timestamp, but it actually seems to be shifting the other way, so
    I don't know. I might be missing something.
    
    Finally, the concept of a `replicaoracle.OracleFactoryFunc` was removed
    and the existing `replicaoracle.OracleFactory` takes its place. We no
    longer need the double-factory approach because the transaction object
    is now passed directly to `Oracle.ChoosePreferredReplica`. This was a
    necessary change, because the process of determining whether a follower
    read can be served now requires transaction information and range
    information (i.e. the closed timestamp policy), so we need to make it in
    the Oracle itself instead of in the OracleFactory. This all seems like a
    simplification anyway.
    
    This is still waiting on changes to the closed timestamp system to be
    able to write end-to-end tests using this new functionality.

--- pkg/kv/kvserver/replica_follower_read.go
+++ pkg/kv/kvserver/replica_follower_read.go
@@ -64,14 +64,10 @@ func (r *Replica) canServeFollowerRead(
 		return pErr
 	}
 
-	ts := ba.Timestamp
-	if ba.Txn != nil {
-		ts.Forward(ba.Txn.MaxTimestamp)
-	}
-
-	maxClosed, _ := r.MaxClosedTimestamp(ctx)
-	canServeFollowerRead := ts.LessEq(maxClosed)
-	tsDiff := ts.GoTime().Sub(maxClosed.GoTime())
+	maxObservableTS := ba.Txn.MaxObservableTimestamp()
+	maxClosed, _ := r.maxClosed(ctx)
+	canServeFollowerRead := maxObservableTS.LessEq(maxClosed)
+	tsDiff := maxObservableTS.GoTime().Sub(maxClosed.GoTime())
 	if !canServeFollowerRead {
 		maxTsStr := "n/a"
 		if ba.Txn != nil {
@@ -81,7 +77,7 @@ func (r *Replica) canServeFollowerRead(
 		// We can't actually serve the read based on the closed timestamp.
 		// Signal the clients that we want an update so that future requests can succeed.
 		r.store.cfg.ClosedTimestamp.Clients.Request(lErr.LeaseHolder.NodeID, r.RangeID)
-		log.Eventf(ctx, "can't serve follower read; closed timestamp too low by: %s; MaxClosedTimestamp: %s ts: %s maxTS: %s",
+		log.Eventf(ctx, "can't serve follower read; closed timestamp too low by: %s; maxClosed: %s ts: %s maxTS: %s",
 			tsDiff, maxClosed, ba.Timestamp, maxTsStr)
 
 		if false {
@@ -105,7 +101,7 @@ func (r *Replica) canServeFollowerRead(
 	return nil
 }
 
-// MaxClosedTimestamp returns the maximum closed timestamp for this range.
+// maxClosed returns the maximum closed timestamp for this range.
 // It is computed as the most recent of the known closed timestamp for the
 // current lease holder for this range as tracked by the closed timestamp
 // subsystem and the start time of the current lease. It is safe to use the
@@ -116,7 +112,7 @@ func (r *Replica) canServeFollowerRead(
 // uses an expiration-based lease. Expiration-based leases do not support the
 // closed timestamp subsystem. A zero-value timestamp will be returned if ok
 // is false.
-func (r *Replica) MaxClosedTimestamp(ctx context.Context) (_ hlc.Timestamp, ok bool) {
+func (r *Replica) maxClosed(ctx context.Context) (_ hlc.Timestamp, ok bool) {
 	r.mu.RLock()
 	lai := r.mu.state.LeaseAppliedIndex
 	lease := *r.mu.state.Lease
