0d314d157ac9eb38d7ea94fdda2c7db1c49bc111
Andrei Matei <andrei@cockroachlabs.com>
2022-02-01 14:46:56 -0500

    mucking with rdtsc

---
+++ pkg/util/timeutil/asm_amd64.s
@@ -0,0 +1,8 @@
+#include "textflag.h"
+
+TEXT ·CPUTicks(SB),NOSPLIT,$0-0
+RDTSC
+SHLQ	$32, DX
+ADDQ	DX, AX
+MOVQ	AX, ret+0(FP)
+RET
--- pkg/util/timeutil/now_test.go
+++ pkg/util/timeutil/now_test.go
@@ -10,10 +10,43 @@
 
 package timeutil
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+	"time"
+	_ "unsafe"
+)
 
 func BenchmarkNow(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		Now()
+	}
+}
+
+//go:linkname cputicks runtime.cputicks
+func cputicks() int64
+
+func CPUTicks() int64
+
+func BenchmarkCpuTicks(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		cputicks()
+	}
+}
+
+func BenchmarkMyTicks(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		CPUTicks()
+	}
+}
+
+func TestSizeXXX(t *testing.T) {
+	t1 := CPUTicks()
+	fmt.Printf("cputicks: %d\n", t1)
+	for i := 0; i < 10; i++ {
+		time.Sleep(time.Second)
+		t2 := CPUTicks()
+		fmt.Printf("after sleep: %d. delta: %d\n", t2, t2-t1)
+		t1 = t2
 	}
 }
