ededd6c084a89f7df84d21a8b5cc2b435f144821
Andrei Matei <andrei@cockroachlabs.com>
2022-02-02 11:57:18 -0500

    no local flow span

--- pkg/sql/distsql/server.go
+++ pkg/sql/distsql/server.go
@@ -254,7 +254,7 @@ func (ds *ServerImpl) setupFlow(
 		// TODO(andrei): localState.IsLocal is not quite the right thing to use.
 		//  If that field is unset, we might still want to create a child span if
 		//  this flow is run synchronously.
-		ctx, sp = ds.Tracer.StartSpanCtx(ctx, opName, tracing.WithParent(parentSpan))
+		// !!! ctx, sp = ds.Tracer.StartSpanCtx(ctx, opName, tracing.WithParent(parentSpan))
 	} else {
 		// We use FollowsFrom because the flow's span outlives the SetupFlow request.
 		ctx, sp = ds.Tracer.StartSpanCtx(
